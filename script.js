f0438238883350842828421c556b6f2eac3b91de
Siphosenkosi Mthulisi Ndhlovu <sipho.ndhlovu@outlook.com>
2017-04-07 17:39:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-var $location = $('#location'),
-    $temp = $('#temp'),
-    $tempMax = $('#tempMax'),
-    $tempMin = $('#tempMin'),
-    $weatherIcon = $('#weatherIcon'),
-    $weatherDesc = $('#weatherDesc'),
-    $humid = $('#humid'),
-    $wind = $('#wind'),
+var $location = document.getElementById('location'),
+    $temp = document.getElementById('temp'),
+    $tempMax = document.getElementById('tempMax'),
+    $tempMin = document.getElementById('tempMin'),
+    $weatherIcon = document.getElementById('weatherIcon'),
+    $weatherDesc = document.getElementById('weatherDesc'),
+    $humid = document.getElementById('humid'),
+    $wind = document.getElementById('#wind'),
     api = "http://api.openweathermap.org/data/2.5/weather\?",
     key = "&appid=979223e5411815d908c275a992b5c234";
 console.log(api);
@@ -24,13 +24,13 @@ function useGeoData(position){
   $.getJSON(api + apiLocator + key, function (json){
     alert(JSON.stringify(json));
     alert('SIPHO');
-    $location.html(json.name + ", " + json.sys.country);
-    $temp.html(json.main.temp);
-    $tempMax.html(json.main.temp_max);
-    $tempMin.html(json.main.temp_min);
-    $weatherIcon.html("<img src='");
-    $weatherDesc.html(json.weather.description);
-    $humid.html(json.main.humidity);
-    $wind.html(json.wind.speed + 'm/s ' + Math.round(json.wind.deg) + "&deg;" )
+    $location.innerHTML = json.name + ", " + json.sys.country;
+    $temp.innerHTML = json.main.temp;
+    $tempMax.innerHTML = json.main.temp_max;
+    $tempMin.innerHTML = json.main.temp_min;
+    $weatherIcon.innerHTML = "<img src='";
+    $weatherDesc.innerHTML = json.weather.description;
+    $humid.innerHTML = json.main.humidity;
+    $wind.innerHTML = json.wind.speed + 'm/s ' + Math.round(json.wind.deg) + "&deg;" ;
   });
 };
