e1d27f49137504aa897aa1cbd0726e83fdb0b06c
Siphosenkosi Mthulisi Ndhlovu <sipho.ndhlovu@outlook.com>
2017-04-08 22:36:50 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
-var $location = document.getElementById('location'),
-    $temp = document.getElementById('temp'),
-    $tempMax = document.getElementById('tempMax'),
-    $tempMin = document.getElementById('tempMin'),
-    $weatherIcon = document.getElementById('weatherIcon'),
-    $weatherDesc = document.getElementById('weatherDesc'),
-    $humid = document.getElementById('humid'),
-    $wind = document.getElementById('#wind'),
+var $location = $('#location'),
+    $temp = $('#temp'),
+    $tempMax = $('#tempMax'),
+    $tempMin = $('#tempMin'),
+    $weatherIcon = $('#weatherIcon'),
+    $weatherMain = $('#weatherMain'),
+    $weatherDesc = $('#weatherDesc'),
+    $humid = $('#humid'),
+    $wind = $('#wind'),
+    $displayContent = $('#displayContent'),
+    $loader = $('#loader'),
     api = "http://api.openweathermap.org/data/2.5/weather\?",
-    key = "&appid=979223e5411815d908c275a992b5c234";
+    key = "&appid=979223e5411815d908c275a992b5c234",
+    units = "&units=metric";
 console.log(api);
 if (navigator.geolocation) {
-  alert('data');
+  console.log('data');
   navigator.geolocation.getCurrentPosition(useGeoData);
 }else{
-  alert('no data');
+  console.log('no data');
 }
 function useGeoData(position){
-  alert('located!');
+  console.log('located!')
   var lon = position.coords.longitude;
   var lat = position.coords.latitude;
-  alert(lon);
+  console.log(lon);
   var apiLocator = "lat=" + lat + "&" + "lon=" + lon;
-  $.getJSON(api + apiLocator + key, function (json){
-    alert(JSON.stringify(json));
-    alert('SIPHO');
-    $location.innerHTML = json.name + ", " + json.sys.country;
-    $temp.innerHTML = json.main.temp;
-    $tempMax.innerHTML = json.main.temp_max;
-    $tempMin.innerHTML = json.main.temp_min;
-    $weatherIcon.innerHTML = "<img src='";
-    $weatherDesc.innerHTML = json.weather.description;
-    $humid.innerHTML = json.main.humidity;
-    $wind.innerHTML = json.wind.speed + 'm/s ' + Math.round(json.wind.deg) + "&deg;" ;
-  });
+  $.getJSON( api + apiLocator + units + key , getWeatherData );
 };
+function getWeatherData (json){
+  console.log(JSON.stringify(json));
+  $location.html(json.name + ", " + json.sys.country);
+  $temp.html(Math.round(json.main.temp) + '<i class="wi wi-celsius"></i>');
+  $tempMax.html(Math.round(json.main.temp_max) + '<i class="wi wi-celsius"></i>');
+  $tempMin.html(Math.round(json.main.temp_min) + '<i class="wi wi-celsius"></i>');
+  $weatherIcon.html("<img src='http://openweathermap.org/img/w/" +json.weather[0].icon + ".png'>");
+  $weatherMain.html(json.weather[0].main + '<br>');
+  $weatherDesc.html(json.weather[0].description);
+  $humid.html(json.main.humidity);
+  $wind.html("<i class='wi wi-wind towards-" + Math.round(json.wind.deg) + "-deg'></i> " + json.wind.speed + 'm/s ');
+  $displayContent.css('display', 'block');
+  $loader.css('display', 'none');
+}
