f0821a1c7160395206628ec563c3b307568153ab
Siphosenkosi Mthulisi Ndhlovu <sipho.ndhlovu@outlook.com>
2017-04-10 10:53:22 +0200

    Location now working
    There are now two optiins for locating. If geolocation is not supported or unavailable, the IP address is used through geoip API

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 var $location = $('#location'),
     $temp = $('#temp'),
     $tempMax = $('#tempMax'),
@@ -5,19 +6,39 @@ var $location = $('#location'),
     $weatherIcon = $('#weatherIcon'),
     $weatherMain = $('#weatherMain'),
     $weatherDesc = $('#weatherDesc'),
+    $barometer = $('#barometer'),
     $humid = $('#humid'),
     $wind = $('#wind'),
+    $sunrise = $('#sunrise'),
+    $sunset = $('#sunset'),
+    $moonrise = $('#moonrise'),
+    $moonset = $('#moonset'),
     $displayContent = $('#displayContent'),
     $loader = $('#loader'),
+    $raindrops = $('.rain_drop'),
+    tempFahr,
+    maxTempFahr,
+    minTempFahr,
     api = "http://api.openweathermap.org/data/2.5/weather\?",
     key = "&appid=979223e5411815d908c275a992b5c234",
-    units = "&units=metric";
-console.log(api);
+    units = "&units=metric",
+    geoip = "http://freegeoip.net/json/?q=";
 if (navigator.geolocation) {
   console.log('data');
-  navigator.geolocation.getCurrentPosition(useGeoData);
+  navigator.geolocation.getCurrentPosition(useGeoData,alternateData);
 }else{
-  console.log('no data');
+  alternateData();
+}
+function alternateData(){
+  $.getJSON('https://api.ipify.org?format=json', function(json){
+    var ipaddr = json.id;
+    $.getJSON(geoip + ipaddr, function(json1){
+      var lat = json1.latitude,
+          lon = json1.longitude;
+      var apiLocator = "lat=" + lat + "&" + "lon=" + lon;
+          $.getJSON( api + apiLocator + units + key , getWeatherData );
+    })
+  })
 }
 function useGeoData(position){
   console.log('located!')
@@ -29,15 +50,55 @@ function useGeoData(position){
 };
 function getWeatherData (json){
   console.log(JSON.stringify(json));
-  $location.html(json.name + ", " + json.sys.country);
-  $temp.html(Math.round(json.main.temp) + '<i class="wi wi-celsius"></i>');
-  $tempMax.html(Math.round(json.main.temp_max) + '<i class="wi wi-celsius"></i>');
-  $tempMin.html(Math.round(json.main.temp_min) + '<i class="wi wi-celsius"></i>');
-  $weatherIcon.html("<img src='http://openweathermap.org/img/w/" +json.weather[0].icon + ".png'>");
-  $weatherMain.html(json.weather[0].main + '<br>');
-  $weatherDesc.html(json.weather[0].description);
-  $humid.html(json.main.humidity);
-  $wind.html("<i class='wi wi-wind towards-" + Math.round(json.wind.deg) + "-deg'></i> " + json.wind.speed + 'm/s ');
-  $displayContent.css('display', 'block');
-  $loader.css('display', 'none');
+  var sunriseTime = new Date(json.sys.sunrise * 1000),
+      sunsetTime = new Date(json.sys.sunset * 1000),
+      temp = Math.round(json.main.temp),
+      maxTemp = Math.round(json.main.temp_max),
+      minTemp = Math.round(json.main.temp_min),
+      icon = json.weather[0].icon,
+      main = json.weather[0].main,
+      description = json.weather[0].description,
+      humidity  = json.main.humidity,
+      windDeg = Math.round(json.wind.deg),
+      windSpeed = json.wind.speed,
+      pressure = Math.round(json.main.pressure) + "hPa",
+      sunrise = function (){
+        var minutes = "";
+        if (sunriseTime.getMinutes() < 10) {
+          minutes = "0" + sunriseTime.getMinutes();
+        }else{
+          minutes = sunriseTime.getMinutes();
+        }
+        return sunriseTime.getHours() + ":" + minutes;
+      },
+      sunset = function(){
+        var minutes = "";
+        if (sunsetTime.getMinutes() < 10) {
+          minutes = "0" + sunsetTime.getMinutes();
+        }else{
+          minutes = sunsetTime.getMinutes();
+        }
+        return sunsetTime.getHours() + ":" + minutes;
+      },
+      cityName = json.name + ", " + json.sys.country;
+      update(cityName, temp, maxTemp, minTemp, icon,  main, description, humidity, windDeg, windSpeed, pressure, sunrise, sunset)
 }
+function update(cityName, temp, maxTemp, minTemp, icon,  main, description, humidity, windDeg, windSpeed, pressure, sunrise, sunset){
+      $location.html(cityName);
+      $temp.html(temp + '<i class="wi wi-celsius"></i>');
+      $tempMax.html(maxTemp + '<i class="wi wi-celsius"></i>');
+      $tempMin.html(minTemp + '<i class="wi wi-celsius"></i>');
+      $weatherIcon.html("<img src='http://openweathermap.org/img/w/" + icon + ".png'>");
+      $weatherMain.html(main);
+      $weatherDesc.html(description);
+      $humid.html(humidity);
+      $wind.html("<i class='wi wi-wind towards-" + windDeg + "-deg'></i> " + windSpeed + 'm/s ');
+      $barometer.html(pressure);
+      $sunrise.html(sunrise());
+      $sunset.html(sunset());
+      $displayContent.css('display', 'block');
+      $loader.css('display', 'none');
+}
+var alterTemp = Number($temp.html().substring(0,2)),
+    altermaxTemp = Number($tempMax.html().substring(0,2)),
+    alterminTemp = Number($tempMin.html().substring(0,2));
